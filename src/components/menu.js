1405b626eaabee7bacb4401c6193a40f0de54c6d
arjun391c <arjun391c@gmail.com>
2020-03-29 23:11:51 +0530

    statespropslifecycleliftup

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,41 +1,80 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import {
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle
+} from "reactstrap";
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+
+        this.state = {
+            selectedDish: null
+        };
+        console.log("menu comp invoked");
+    }
+
+    componentDidMount() {
+        console.log("menu comp didmount invoked");
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return ( <
+                Card >
+                <
+                CardImg top src = { dish.image }
+                alt = { dish.name }
+                />{" "} <
+                CardBody >
+                <
+                CardTitle > { dish.name } < /CardTitle>{" "} <
+                CardText > { dish.description } < /CardText>{" "} <
+                /CardBody>{" "} <
+                /Card>
+            );
+        else return <div > < /div>;
     }
 
     render() {
         const menu = this.props.dishes.map(dish => {
             return ( <
-                div key = { dish.id }
-                className = "col-12 mt-5" >
+                div className = "col-12 col-md-5 m-1" >
                 <
-                Media tag = "li" >
+                Card key = { dish.id }
+                onClick = {
+                    () => this.onDishSelect(dish) } >
                 <
-                Media left middle >
-                <
-                Media object src = { dish.image }
+                CardImg width = "100%"
+                src = { dish.image }
                 alt = { dish.name }
-                /> {""}{" "} <
-                /Media>{" "} { "" } { " " } <
-                Media body className = "ml-5" >
+                />{" "} <
+                CardImgOverlay >
                 <
-                Media heading > { dish.name } < /Media> <p> {dish.description} </p > { " " } <
-                /Media>{" "} <
-                /Media>{" "} <
+                CardTitle > { dish.name } < /CardTitle>{" "} <
+                /CardImgOverlay>{" "} <
+                /Card>{" "} <
                 /div>
             );
         });
+        console.log("menu  render invoked");
 
         return ( <
             div className = "container" >
             <
+            div className = "row" > { menu } < /div>{" "} <
             div className = "row" >
             <
-            Media list > { menu } < /Media>{" "} <
+            div className = "col-12 col-md-5 m-1" > { " " } { this.renderDish(this.state.selectedDish) } { " " } <
+            /div>{" "} <
             /div>{" "} <
             /div>
         );
