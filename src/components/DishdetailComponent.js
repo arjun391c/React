b191c304c10d97c0c6cce54339af617a16b24bc9
arjun391c <arjun391c@gmail.com>
2020-03-30 15:01:51 +0530

    functionalcomp

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
     Card,
     CardImg,
@@ -9,62 +9,61 @@ import {
     CardHeader
 } from "reactstrap";
 
-class DishDetail extends Component {
-    renderComments(comments) {
-        if (comments != null)
-            return comments.map(comment => {
-                return ( <
-                    li >
-                    <
-                    p > { comment.comment } < /p>{" "} <
-                    p >
-                    --{ comment.author }, { " " } {
-                        new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "short",
-                            day: "2-digit"
-                        }).format(new Date(Date.parse(comment.date)))
-                    } { " " } <
-                    /p>{" "} <
-                    /li>
-                );
-            });
-        else return <div > < /div>;
-    }
-
-    render() {
-        const dish = this.props.dish;
-
-        if (dish != null) {
-            const comment = this.renderComments(dish.comments);
+function renderComments(comments) {
+    if (comments != null)
+        return comments.map(comment => {
             return ( <
-                div className = "container" >
+                li >
                 <
-                div className = "row " >
-                <
-                div className = "col-12 col-md-5 m-1" >
-                <
-                Card >
-                <
-                CardImg src = { dish.image }
-                alt = { dish.name }
-                />{" "} <
-                CardBody >
-                <
-                CardTitle > { dish.name } < /CardTitle>{" "} <
-                CardText > { dish.description } < /CardText>{" "} <
-                /CardBody>{" "} <
-                /Card>{" "} <
-                /div>{" "} <
-                div className = "col-12 col-md-5 m-1" >
-                <
-                h4 > Comments < /h4> <ul className="list-unstyled"> {comment}</ul > { " " } <
-                /div>{" "} <
-                /div>{" "} <
-                /div>
+                p > { comment.comment } < /p>{" "} <
+                p >
+                --{ comment.author }, { " " } {
+                    new Intl.DateTimeFormat("en-US", {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit"
+                    }).format(new Date(Date.parse(comment.date)))
+                } { " " } <
+                /p>{" "} <
+                /li>
             );
-        } else return <div > < /div>;
-    }
+        });
+    else return <div > < /div>;
 }
+
+const DishDetail = selected => {
+    console.log("dishdetail render");
+    const dish = selected.dish;
+
+    if (dish != null) {
+        const comment = renderComments(dish.comments);
+        return ( <
+            div className = "container" >
+            <
+            div className = "row " >
+            <
+            div className = "col-12 col-md-5 m-1" >
+            <
+            Card >
+            <
+            CardImg src = { dish.image }
+            alt = { dish.name }
+            />{" "} <
+            CardBody >
+            <
+            CardTitle > { dish.name } < /CardTitle>{" "} <
+            CardText > { dish.description } < /CardText>{" "} <
+            /CardBody>{" "} <
+            /Card>{" "} <
+            /div>{" "} <
+            div className = "col-12 col-md-5 m-1" >
+            <
+            h4 > Comments < /h4> <ul className="list-unstyled"> {comment}</ul > { " " } <
+            /div>{" "} <
+            /div>{" "} <
+            /div>
+        );
+    } else return <div > < /div>;
+};
 
 export default DishDetail;
